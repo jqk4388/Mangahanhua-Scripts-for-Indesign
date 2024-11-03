746141fb46bae51d5d0f4bec8ce8523164cd374c
几千块 <hhhi5@qq.com>
2024-11-04 03:35:23 +0800

    Delete Unbreak Text.jsx

--- Unbreak Text.jsx
+++
@@ -1,5 +0,0 @@
-var myFrame = app.selection[0] // get first item of selection
-
-myFrame.contents = myFrame.contents.toString().replace(/\s/g, ' ') // replace ANY whitespace with a single space
-myFrame.texts.everyItem().leading = Leading.AUTO // reset all leading to default
-
