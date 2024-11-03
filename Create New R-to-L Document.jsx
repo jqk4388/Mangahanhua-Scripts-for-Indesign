b1e36ecdfd82955119b919dfdfe89e1d8f97247c
几千块 <hhhi5@qq.com>
2024-11-04 03:35:14 +0800

    Delete Create New R-to-L Document.jsx

--- Create New R-to-L Document.jsx
+++
@@ -1,5 +0,0 @@
-// This creates a new document with the binding direction set Right to Left
-app.documents.add(
-            {
-                documentPreferences: {pageBinding: PageBindingOptions.RIGHT_TO_LEFT}
-            })
